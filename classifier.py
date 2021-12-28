2a10afb13a3d7bbd889f03c3d21060a4e078d847
Biye Li <libiye@gmail.com>
2021-12-28 19:28:26 +0800

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -4,18 +4,18 @@ import argparse
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+import numpy as np
 from transformers import BertTokenizerFast, BertForSequenceClassification
 from transformers import Trainer, TrainingArguments
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
+import csv
 
 class CovidDataset(Dataset):
 
-    def __init__(self, csv_file):
-    def __init__(self, csv_file, model):
-        frame = pd.read_csv(csv_file, encoding='latin-1')
+    def __init__(self, frame):
         self.frame = frame
         tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-        tokenizer = BertTokenizerFast.from_pretrained(model)
         self.tweets = frame.iloc[:,4].tolist()
         self.tweets = tokenizer(self.tweets, return_tensors='pt', 
                                 padding=True, truncation=True)
@@ -38,7 +38,20 @@ class CovidDataset(Dataset):
 
         return ret
 
+def load_dataset(csv_file, mode='train', test_size=0.2):
+    frame = pd.read_csv(csv_file, encoding='latin-1')
+    
+    if mode == 'train':
+        train, test = train_test_split(frame, test_size)
+        train_set = CovidDataset(train)
+        test_set = CovidDataset(test)
+    else:
+        train_set = None
+        test_set = CovidDataset(frame)
+    
+    return {'train': train_set, 'test': test_set}
 
+    
 def compute_metrics(pred):
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
@@ -55,22 +68,17 @@ def compute_metrics(pred):
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices=['train', 'eval'])
+    parser.add_argument('mode', choices=['train', 'eval', 'predict'])
     
     parser.add_argument('--import_model', default='bert-base-uncased')
     
-    parser.add_argument('--dataset_dir', default='.')
+    parser.add_argument('--dataset_path', default='.')
     parser.add_argument('--output_dir', default='.')
     
     args = parser.parse_args()
-    
-    ds_dir = args.dataset_dir
-    training_set = CovidDataset('{}/Corona_NLP_train.csv'.format(ds_dir))
-    test_set = CovidDataset('{}/Corona_NLP_test.csv'.format(ds_dir))
-    training_set = CovidDataset('{}/Corona_NLP_train.csv'.format(ds_dir), args.import_model)
-    test_set = CovidDataset('{}/Corona_NLP_test.csv'.format(ds_dir), args.import_model)
+    ds_path = args.dataset_path
+    dataset = load_dataset(ds_path, args.mode)
 
-    training_set[0]
     training_args = TrainingArguments(
         output_dir='{}/checkpoints'.format(args.output_dir),
         num_train_epochs=3,
@@ -90,11 +98,31 @@ if __name__ == '__main__':
         model=model,
         args=training_args,
         compute_metrics=compute_metrics,
-        train_dataset=training_set,
-        eval_dataset=test_set
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test']
     )
 
     if args.mode == 'train':
         trainer.train()
     elif args.mode == 'eval':
         trainer.evaluate()
+    elif args.mode == 'predict':
+        res = trainer.predict(dataset['test'])
+        #for row in res.predictions:
+        #    print(np.argmax(row))
+        idx = np.argmax(res.predictions, 1)
+        
+        label_map = { 0: 'Extremely Positive',
+                      1: 'Positive',
+                      2: 'Neutral',
+                      3: 'Negative',
+                      4: 'Extremely Negative' }
+        
+        labels = []
+        for i in idx:
+            labels.append([label_map[i]])
+        
+        with open(args.output_dir + '/result.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(labels)
+        
